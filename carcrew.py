01ab23a167b342a19a00e9ff13f913c9327e0bd6
saooms <saooms@hotmail.com>
2025-06-25 12:19:48 +0200

    used model to predict actions

--- carcrew.py
+++ carcrew.py
@@ -1,8 +1,9 @@
 import client
 import math
+import numpy as np
 # from keras.models import load_model
 from tensorflow.keras.models import load_model
-
+import joblib
 ##
 # Inputs:  
 #   See Class "ServerState" fn fancyout for selected inputs
@@ -22,10 +23,29 @@ class maxVerstapte:
     def __init__(self):
         self.Client = client.Client
         self.model = load_model("model/torcs_ai_model.h5")
+        self.scaler = joblib.load("model/torcs_scaler.save")
 
     def drive(self, c):
         S, R = c.S.d, c.R.d
         
+        inputs = np.ndarray((22,), dtype=np.float64)
+        # inputs[0] = R['accel']
+        # inputs[1] = R['brake']
+        # inputs[2] = R['steer']
+        inputs[0] = S['speedX']
+        inputs[1] = S['trackPos']
+        inputs[2] = S['angle']
+        inputs[3:] = S['track']
+        scaled_inputs = self.scaler.transform([inputs])
+
+        predictions = self.model.predict(scaled_inputs, verbose=0).flatten()
+
+        print(f"{predictions[0]}, {round(predictions[1])}, {predictions[2]}")
+
+        R['accel'] = predictions[0]
+        R['brake'] = round(predictions[1])
+        R['steer'] = predictions[2]
+
         # Automatic Transmission
         R['gear'] = 1
         if S['speedX'] > 50:
